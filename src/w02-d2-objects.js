081dc5a764756b4945dec1930d1992d11fdddf38
Matthew Gibbons <matthew.gibbons@multiverse.io>
2023-05-03 08:44:26 +0100

    W02D2

---
+++ src/w02-d2-objects.js
@@ -0,0 +1,26 @@
+const music_collection = [
+	{
+		title: "Led Zeppelin IV",
+		release_date: "8 November 1971",
+		genre: "Hard rock, heavy metal",
+		rating: 11,
+		tracks: [
+			"black dog",
+			"rock and roll",
+			"the battle of evermore",
+			"stairway to heaven",
+		],
+	},
+];
+
+music_collection;
+
+const album = music_collection[0];
+
+const title = album.title;
+
+title;
+
+const track = album.tracks[0];
+
+track;
